5232a6fc6ed1273aefb9a0512661f749aaa9afc1
Vyacheslav <52282118+SlavikArt@users.noreply.github.com>
2023-05-09 23:18:59 +0300

    Добавьте файлы проекта.

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Homework_08.05.2023
+Д/з: dynamic_cast
+
+<a href="https://gist.github.com/SlavikArt/8c3e02c189ae4470fcb35465c300c8d5">Gist - страница со всеми кодами проектов</a>
+
+* [main](main)
+
+<p align="center">
+    <h2>Main</h2>
+    <p></p>
+    <img src="images/main.png">
+</p>
---
+++ main/main.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+class Animal
+{
+public:
+    virtual void Speak() = 0;
+};
+
+class Frog : public Animal
+{
+public:
+    void Speak()
+    {
+        std::cout << "Ribbit Ribbit\n";
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    void Speak()
+    {
+        std::cout << "Woof Woof\n";
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    void Speak()
+    {
+        std::cout << "Meow Meow\n";
+    }
+
+    void Scratch(bool isHomeworkDone)
+    {
+        if (isHomeworkDone)
+            std::cout << "The cat doesn't scratch\n";
+        else
+            std::cout << "The cat scratches\n";
+    }
+
+    void Purr()
+    {
+        std::cout << "The cat purrs\n";
+    }
+};
+
+int main()
+{
+    Animal* animals[3];
+
+    animals[0] = new Frog();
+    animals[1] = new Dog();
+    animals[2] = new Cat();
+
+    for (int i = 0; i < 3; i++)
+        animals[i]->Speak();
+
+    Cat* cat = dynamic_cast<Cat*>(animals[2]);
+
+    cat->Scratch(false);
+    cat->Purr();
+
+    for (int i = 0; i < 3; i++)
+        delete animals[i];
+}
